cac3a4036baa97679fbdbd740123cee40b458753
ELMIR Mohammed <115789886+medelmir@users.noreply.github.com>
2024-01-22 10:55:29 +0100

    fix change again

--- chip8/src/main.c
+++ chip8/src/main.c
@@ -113,7 +113,17 @@ while(1)
                     SDL_RenderFillRect(renderer, &r);
                 }
             }
-        
+        if (chip8.registres.delay_timer > 0)
+        {
+            Sleep(100);
+            chip8.registres.delay_timer--;
+            printf("delay");
+        }
+        if (chip8.registres.sound_timer > 0)
+        {
+            Beep(1300, 100 * chip8.registres.sound_timer);
+            chip8.registres.sound_timer = 0;
+        }
             }
         SDL_RenderPresent(renderer);
         
